37aa764f0ea46e9b22562e3f3e8d020f277c7eb7
Insafsuz <in7afsuz@gmail.com>
2022-11-10 17:44:25 +0300

    initialize project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FE5F1E',
+        white: '#ffffff',
+        black: '#000000',
+        gray: {
+          500: '#2C2C2C',
+          700: '#282828',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
